574316849d455284bd61e090894a0772fb70a321
fkyohei <kyohei19921016@gmail.com>
2015-06-18 16:12:17 +0900

    update app

--- SwiftNews/SwiftNews/DetailController.swift
+++ SwiftNews/SwiftNews/DetailController.swift
@@ -7,7 +7,57 @@
 //
 
 import UIKit
+import Social
 
 class DetailController: UIViewController {
+    @IBOutlet weak var webView: UIWebView!
+    // エントリー
+    var entry = NSDictionary()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // webビューでURLを読み込む
+        var url = NSURL(string: self.entry["link"] as! String)!
+        var request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
+    
+    @IBAction func twitter(sender: AnyObject) {
+        // Twitterへの投稿が行えるか確認
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            // コントローラを作る
+            var controller = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            // リンクを追加
+            var link = entry["link"] as! String
+            var url = NSURL(string: link)
+            controller.addURL(url)
+            
+            // テキストを追加
+            var title = entry["title"] as! String
+            controller.setInitialText(title)
+            
+            presentViewController(controller, animated: true, completion: {})
+        }
+    }
+    
+    @IBAction func facebook(sender: AnyObject) {
+        // Facebookはの投稿が行えるか確認
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            // コントローラーを作る
+            var controller = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            
+            // リンクを追加
+            var link = entry["link"] as! String
+            var url = NSURL(string: link)
+            controller.addURL(url)
+            
+            // テキストを追加
+            var title = entry["title"] as! String
+            controller.setInitialText(title)
+            
+            presentViewController(controller, animated: true, completion: {})
+        }
+    }
 }
--- SwiftNews/SwiftNews/ViewController.swift
+++ SwiftNews/SwiftNews/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UITableViewController {
     // ニュースのアドレス
-    let newsUrlString = "http://www.apple.com"
+    let newsUrlString = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://rss.itmedia.co.jp/rss/2.0/news_bursts.xml&num=8"
+    // エントリーの配列
+    var entries = NSArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +26,36 @@ class ViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // テーブルの行数を返す
-        return 5
+        return entries.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // セルを取得する
-        var cell = tableView.dequeueReusableCellWithIdentifier("news") as! UITableViewCell
+        var cell = tableView.dequeueReusableCellWithIdentifier("news") as? UITableViewCell
         
+        // エントリーを取得する
+        var entry = entries[indexPath.row] as! NSDictionary
+
         // セルにテキストを設定する
-        cell.textLabel!.text = "Swift News"
+        cell?.textLabel!.text = entry["title"] as? String
         
-        return cell
+        return cell!
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // セグエを実行する
+        performSegueWithIdentifier("detail", sender: entries[indexPath.row])
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // detailセグエの場合
+        if segue.identifier == "detail" {
+            // DetailControllerを取得する
+            var detailContoroller = segue.destinationViewController as! DetailController
+            
+            //エントリーを設定する
+            detailContoroller.entry = sender as! NSDictionary
+        }
     }
 
     @IBAction func reflesh(sender: AnyObject) {
@@ -44,10 +65,26 @@ class ViewController: UITableViewController {
         // データをダウンロードする
         var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: {
             data, response, error in
-            // ダウンロードサイズを表示する（テスト用）
-            println("done, length \(data.length)")
+            // JSONデータを辞書に変換する
+            var dict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+            
+            // /response/feed/entriesを取得
+            if var responseData = dict["responseData"] as? NSDictionary {
+                if var feed = responseData["feed"] as? NSDictionary {
+                    if var entries = feed["entries"] as? NSArray {
+                        // エントリーの配列を設定する
+                        self.entries = entries
+                    }
+                }
+            }
         })
         task.resume()
+        
+        // テーブルビューを更新するため、メインスレッドにスイッチする
+        dispatch_async(dispatch_get_main_queue(), {
+            // テーブルビューの更新をする
+            self.tableView.reloadData()
+        })
     }
 }
 
