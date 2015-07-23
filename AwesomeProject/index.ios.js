74d7df4fa159b383e1f72779f4d9bbc02b973213
fkyohei <kyohei19921016@gmail.com>
2015-07-23 12:00:58 +0900

    API呼び出ししてデータ表示まで

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -5,49 +5,103 @@
 'use strict';
 
 var React = require('react-native');
+
+// API URL
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 var AwesomeProject = React.createClass({
+  // APIでデータ取得時のnullチェック
+  getInitialState: function() {
+    return {
+      movies: null,
+    };
+  },
+  // 読み込み後最初の呼び出し
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  // 描画メイン処理
   render: function() {
+    // データ空チェック
+    if(!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+  // データ空時の描画
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          Loading movies...
         </Text>
       </View>
     );
-  }
+  },
+  // データ取得後の描画
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+  // データ取得、セット
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  rightContainer: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
     fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
+  year: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
