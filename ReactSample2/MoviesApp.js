62ed746412c5e048c5010b5bbb8d457c542bc861
fkyohei <kyohei19921016@gmail.com>
2015-07-23 13:05:49 +0900

    Reactサンプルアプリ追加

---
+++ ReactSample2/MoviesApp.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet,
+} = React;
+
+var MoviesApp = 
+
+AppRegistry.registerComponent('MoviesApp', () => MoviesApp);
+
+module.exports = MoviesApp;
---
+++ ReactSample2/index.ios.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+} = React;
+
+// 初期呼び出しファイルを設定
+var ReactSample2 = require('./MovieApp.js');
+
+AppRegistry.registerComponent('ReactSample2', () => ReactSample2);
